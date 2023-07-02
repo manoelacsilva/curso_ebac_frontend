c19866ff841a5329592ecb2ed6d2ed7976dbb293
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-07-02 17:23:25 -0300

    removendo da tela a mensagem de sucesso anterior

--- HTML com JS/main.js
+++ HTML com JS/main.js
@@ -3,34 +3,44 @@ const campoB = document.getElementById('campo-b');
 let formEhValido = false;
 
 function validaValor() {
-    return parseInt(document.getElementById('campo-a').value) < parseInt(document.getElementById('campo-b').value);
+    const campoA = document.getElementById('campo-a');
+    const mensagemSucesso = document.querySelector('.success-message');
+    const campoAValue = parseInt(campoA.value);
+    const campoBValue = parseInt(campoB.value);
+
+    if (campoAValue < campoBValue) {
+        mensagemSucesso.style.display = 'block';
+        mensagemSucesso.innerHTML = `O número <b>${campoBValue}</b> é maior que o número <b>${campoAValue}</b>`;
+        return true;
+    } else {
+        mensagemSucesso.style.display = 'none';
+        return false;
+    }
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const campoA = document.getElementById('campo-a');
-    const mensagemSucesso = `O número <b>${campoB.value}</b> é maior que o número <b>${campoA.value}`;
-
     formEhValido = validaValor();
     if (formEhValido) {
+        const campoA = document.getElementById('campo-a');
+        const campoB = document.getElementById('campo-b');
         const containerMensagemSucesso = document.querySelector('.success-message');
-        containerMensagemSucesso.innerHTML = mensagemSucesso;
-        containerMensagemSucesso.style.display = 'block';
 
         campoA.value = '';
         campoB.value = '';
+
+        containerMensagemSucesso.style.display = 'block';
         alert('Tudo certo! Seu formulário está válido.')
     } else {
         campoB.style.border = '1px solid red';
         document.querySelector('.error-message').style.display = 'block';
-        alert('Algo está errado! Seu formulário está inválido.')
+        alert('Algo está errado! Seu formulário está inválido.');
     }
-})
+});
 
-campoB.addEventListener('keyup', function(e){
-    console.log(e.target.value);
-    formEhValido = validaValor(e.target.value);
+campoB.addEventListener('keyup', function() {
+    formEhValido = validaValor();
 
     if (!formEhValido) {
         campoB.classList.add('error');
@@ -39,4 +49,4 @@ campoB.addEventListener('keyup', function(e){
         campoB.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
     }
-})
+});
