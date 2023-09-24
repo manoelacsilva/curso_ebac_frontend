1ef5bacf8b7bcbaf6b905908661f71dc88829b72
Manoela Coelho Silva <manoelacs62@gmail.com>
2023-09-24 20:52:52 -0300

    usando o comando npx tsc

--- src/tarefa.js
+++ src/tarefa.js
@@ -2,17 +2,13 @@
 // Utilizando o TypeScript e a tipagem, escreva duas funções:
 // Uma função de multiplicação, que deverá receber como argumentos dois números e retornar a multiplicação deles.
 // E uma função de saudação, que deverá receber um nome e retornar a concatenação 'Olá, ' + nome.
-//class Saudacao {
-//   constructor( public nome: string) {
-//   }
-//   dizOla(): string {
-//       return `Olá, ${this.nome}`;
-//  }
-//};
-//const pessoa = new Saudacao('Manoela');
-//const saudacao = pessoa.dizOla();
-//console.log(saudacao);
 function multiplicacao(a, b) {
     return a * b;
 }
+;
 console.log(multiplicacao(2, 3));
+function saudacao(nome) {
+    return 'Olá, ' + nome;
+}
+;
+console.log(saudacao('Manoela'));
